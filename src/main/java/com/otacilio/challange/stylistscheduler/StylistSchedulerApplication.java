c219bcaa4424ff9ebfebce12757c291b64b3bd85
Otacilio Lacerda <otacilio.freitas.de.lacerda@zlabels.de>
2019-01-25 16:27:23 +0100

    Init: project structure and requirements

---
+++ src/main/java/com/otacilio/challange/stylistscheduler/StylistSchedulerApplication.java
@@ -0,0 +1,14 @@
+package com.otacilio.challange.stylistscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylistSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StylistSchedulerApplication.class, args);
+    }
+
+}
+
