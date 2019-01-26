51cdab49834793f410b79c30109dda51164ee01b
Otacilio Lacerda <otacilio.freitas.de.lacerda@zlabels.de>
2019-01-26 20:32:59 +0100

    Remove unused import

--- src/main/java/com/otacilio/challange/stylistscheduler/repository/TimeSlotRepository.java
+++ src/main/java/com/otacilio/challange/stylistscheduler/repository/TimeSlotRepository.java
@@ -2,6 +2,7 @@ package com.otacilio.challange.stylistscheduler.repository;
 
 import com.otacilio.challange.stylistscheduler.model.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
